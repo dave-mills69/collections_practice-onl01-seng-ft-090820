82805dd57dc94039c5c691c712a8f8483580624e
David Mills <david_mills109@yahoo.com>
2020-09-17 15:17:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,119 +1,55 @@
-def sort_array_asc (array)
-  array.sort
+  
+def sort_array_asc(integers)
+  integers.sort
 end
 
-
-
-def sort_array_desc (array)
-  array.sort {|x,y| y <=> x }
+def sort_array_desc(integers)
+  integers.sort {|first_num, second_num| second_num <=> first_num}
 end
 
-
-
-def swap_elements (array)
-  array[1], array[2] = array[2], array[1]
-  array
+def sort_array_char_count(strings)
+  strings.sort {|left, right| left.length <=> right.length}
 end
 
-
-
-def reverse_array (array)
-  array.reverse
+def swap_elements(strings)
+  strings[1], strings[2] = strings[2], strings[1]
+  return strings
 end
 
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  return array
+end
 
+def reverse_array(integers)
+  new_array = integers.reverse
+  new_array
+end
 
-def kesha_maker (array)
-  array.collect do |element|
-    element[2] = "$"
-    element
+def kesha_maker(array)
+  kesha = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    kesha << word_array.join
   end
+  kesha
 end
 
+def find_a(array)
+  array.select{|string| string.start_with?("a")}
+end
 
+def sum_array(integers)
+  integers.inject{|sum, n| sum + n}
+end
 
-def greater_and_less_than_10 (array)
-  hash = {
-    "greater_than_10" => [],
-    "less_than_10" => []
-  }
-  array.each do |number|
-    if number > 10 
-      hash["greater_than_10"] << number 
+def add_s(array)
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
     else
-      hash["less_than_10"] << number
-    end
-  end
-  hash
-end
-
-
-
-def find_winners (hash)
-  winners = []
-  hash.each do |name, status| 
-    winners << name if status == "winner"
-  end
-  winners
-end
-
-
-
-def find_a (array)
-  array.select do |word|
-    word.chr == "a"
-  end
-end
-
-
-
-def sum_array (array)
-  array.inject do |sum, num|
-    sum + num
-  end
-end
-
-
-
-def add_s (array)
-  array.collect.with_index do |word, index|
-    if index == 1 
-      word 
-    else 
-      word + "s"
+      string << "s"
     end
   end
 end
-
-
-
-def count_words (words)
-  story_count = Hash.new
-
-  words.split.each do |word|
-    if story_count.include?(word)
-      story_count[word] = story_count[word] + 1
-    else
-      story_count[word] = 1
-    end
-  end
-
-  story_count
-end
-
-
-
-def organize_songs_by_artist (tracks)
-  hash = {}
-  tracks.each do |name|
-    artist = name.split(" - ")[0]
-    trackname = name.split(" - ")[1]
-
-    if hash.include?(artist)
-      hash[artist] << trackname
-    else
-      hash[artist] = [] << trackname
-    end
-  end
-  hash
-end 
